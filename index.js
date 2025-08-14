519c243a3830a5d57ca809d234cd6f87b61b21a0
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-08-14 20:44:28 +0530

    index.js added

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
+
 const app = express();
 
 require("dotenv").config();
+const PORT = process.env.PORT || 8000;
 
-const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+  res.send("Hello World!");
+});
+
+app.get("/data", (req, res) => {
+  const data = { name: "John Doe", age: 30, city: "New York" };
+  res.json(data);
 });
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
